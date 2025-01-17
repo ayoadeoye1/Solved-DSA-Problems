74901ffb19400cb50293608361c0b1b044e195fe
ayoadeoye1 <ayoadeoye.isaac@gmail.com>
2025-01-17 09:44:07 +0100

    feat: invert tree JS

--- JS/easy.js
+++ JS/easy.js
@@ -223,3 +223,22 @@ var majorityElement = function(nums) {
   }
   return candidate;
 };
+
+/**
+ * 
+ * @name Invert-Tree
+ * 
+ * @description Invert-Binary-Tree
+ * 
+ * 
+ */
+var invertTree = function(root) {
+  if(!root){
+      return root;
+  }
+  let left = invertTree(root.left);
+  let right = invertTree(root.right);
+  root.left = right;
+  root.right = left;
+  return root;
+};
